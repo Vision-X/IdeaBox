64379135ab62838ac6a93ac7d31a62308e914c83
Jonathan Beckman <jbecks.media@gmail.com>
2017-05-22 18:56:26 -0600

    fix enter button functionality

--- script.js
+++ script.js
@@ -11,31 +11,38 @@ $('#save_btn').on('click', function() {
   var body = $('#idea-body').val();
 
   ideaCard();
+  clearInputs();
 
-  title.val('');
-  body.val('');
 
 });
 
+
 function ideaCard() {
 
-  $('.idea-stage').prepend(`
-  <div class="delete-div">
-    <div class="title-delete">
-      <h2>${#idea-title}</h2>
-      <button class="deletebutton">
-        <img type="image" name="delete" id="delete"  src="assets/delete.svg">
-      </button>
-    </div>
-    <p class="body-div">${idea-body}</p>
-    <div class="vote-and-quality">
-      <button class="upvote">
-        <img type="image" name="upvote" id="upvote" src="assets/upvote.svg"></button>
-      <button class="downvote">
-        <img type="image" name="downvote" id="downvote" src="assets/downvote.svg"></button>
-      <p name="quality" id="quality">${qualities}</p>
-    </div>
-    <hr>
-  </div>
-  `);
+  var title = $('#idea-title').val();
+  var body = $('#idea-body').val();
+
+  var injection = `<div class="delete-div">
+      <div class="title-delete">
+        <h2>${title}</h2>
+        <button class="deletebutton">
+          <img type="image" name="delete" id="delete"  src="assets/delete.svg">
+        </button>
+      </div>
+      <p class="body-div">${body}</p>
+      <div class="vote-and-quality">
+        <button class="upvote">
+          <img type="image" name="upvote" id="upvote" src="assets/upvote.svg"></button>
+        <button class="downvote">
+          <img type="image" name="downvote" id="downvote" src="assets/downvote.svg"></button>
+        <p name="quality" id="quality">qualities</p>
+      </div>
+      <hr>
+    </div>`;
+$('.idea-stage').prepend(injection);
+}
+
+function clearInputs() {
+  $('#idea-title').val('');
+  $('#idea-body').val('');
 }
