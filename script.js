62cfb32a83825501635004e19f67edbd2d3ccf46
Jonathan Beckman <jbecks.media@gmail.com>
2017-05-24 15:46:28 -0600

    get info back from local storage

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-getIdeaFromStorage();
-// altGetIdea();
-var newIdea = []
 
+var cardPlace = $('.idea-stage');
+var newIdea = [];
+
+getIdeaFromStorage();
 
 $('#save_btn').on('click', function(event) {
   event.preventDefault();
 
-  var title = $('#idea-title').val();
-  var body = $('#idea-body').val();
-  var id = Date.now();
-  var idea = new Idea(title, body, id);
+  var inputTitle = $('#idea-title').val();
+  var inputBody = $('#idea-body').val();
+  // var id = Date.now();
+  var idea = new Idea(inputTitle, inputBody);
 
   Idea();
   ideaCard();
-  // getKey();
-  // localStore();
+  localStore();
   clearInputs();
 });
 
@@ -25,24 +25,6 @@ $('.idea-stage').on('click', '.upvote', upVote);
 
 $('.idea-stage').on('click', '.downvote', downVote);
 
-
-function getIdeaFromStorage() {
-
-  if (localStorage.getItem($('#idea-title').val())) {
-      var storedItem = localStorage.getItem($('#idea-title').val());
-      newIdea = JSON.parse(storedItem);
-      console.log('parsed items: ' + newIdea);
-
-      newIdea.forEach(function(item) {
-        // var inputNode = //title and body from card
-
-        $('.idea-stage').append(inputNode);
-
-      })
-  } else {
-    console.log('There is nothing locally stored');
-  }
-}
 
 // function altGetIdea() {
 //
@@ -55,58 +37,49 @@ function getIdeaFromStorage() {
 //       }
 // }
 
-function Idea(title, body, id) {
+function Idea(title, body) {
 
   this.title = title
   this.body = body
-  this.id = id
+  this.id = Date.now();
   this.quality = 'swill'
 }
 
-function ideaCard() {
-
-  var title = $('#idea-title').val();
-  var body = $('#idea-body').val();
-  var id = Date.now();
-  console.log('id from ideaCard()' + id);
+function ideaCard(title, body) {
+  // var title = idea.title;
+  // var body = idea.body;
+  // var id = idea.id;
+  var inputTitle = $('#idea-title').val() || title;
+  var inputBody = $('#idea-body').val() || body;
+  // var id = Date.now();
+  // console.log('id from ideaCard(): ' + id);
+  var newCard = new Idea(inputTitle, inputBody);
 
   var injection = `
-    <div class="delete-div" id="${id}">
+    <div class="delete-div">
       <div class="title-delete">
-        <h2 contenteditable="true">${title}</h2>
+        <h2 contenteditable="true">${inputTitle}</h2>
         <button class="deletebutton">
         </button>
       </div>
-      <p class="body-div" contenteditable="true">${body}</p>
+      <p class="body-div" contenteditable="true">${inputBody}</p>
       <div class="vote-and-quality">
         <button id="upvote">
         </button>
         <button id="downvote">
         </button>
-        <p name="quality" id="quality">Quality: ${quality}</p>
+        <p name="quality" id="quality">Quality: ${Idea.quality}</p>
       </div>
       <hr>
     </div>`;
 
-$('.idea-stage').prepend(injection);
-localStore(id);
+cardPlace.prepend(injection);
+newIdea.push(newCard);
 };
 
-// function getKey(){
-//   JSON.stringify($('#idea-title').val())
-// }
+function localStore() {
 
-function localStore(id) {
-  var newObj = new Object();
-    newObj.title = $('#idea-title').val(),
-    newObj.body = $('#idea-body').val();
-    console.log(newObj);
-
-  newIdea.push(newObj);
-  console.log('after push: ' + newIdea);
-  console.log('id from localStore()' + id);
-
-  localStorage.setItem(id, JSON.stringify(newObj));
+  localStorage.setItem('newIdea', JSON.stringify(newIdea));
 };
 
 function clearInputs() {
@@ -114,18 +87,32 @@ function clearInputs() {
   $('#idea-body').val('');
 }
 
+function getIdeaFromStorage() {
+
+  if (localStorage.getItem('newIdea')) {
+      var storedItem = JSON.parse(localStorage.getItem('newIdea'));
+      console.log('parsed items: ' + storedItem);
+
+      storedItem.forEach(function(item) {
+        var inputNode = ideaCard(item.title, item.body);
+        cardPlace.append(inputNode);
+      });
+  } else {
+    console.log('There is nothing locally stored');
+  }
+}
+
 function deleteCard() {
   $(this).closest('.delete-div').remove();
   localRemove();
 };
 
-function localRemove() {
+function localRemove(id) {
+  var grabId = $(this).closest();
+  console.log(grabId)
+  localStorage.removeItem(grabId);
 
 };
-
-function localRetrieve() {
-
-}
 
 function upVote() {
 
